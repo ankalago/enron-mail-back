4833b168ea566432e017dd84901f9503c76ee796
Paúl Jácome <ankalago@gmail.com>
2023-01-31 19:18:50 -0500

    add cors

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"net/http"
 )
 
@@ -14,6 +15,14 @@ func main() {
 	feed := entities.New()
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 	r.Route("/feed", func(r chi.Router) {
 		r.Get("/", httpd.GetFeed(feed))
 		r.Get("/{query}", httpd.GetFeed(feed))
